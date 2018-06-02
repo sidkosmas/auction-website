33ee043d82b6dab30d885ca3f6081433091aa84b
GeorgeRaf <hergath@gmail.com>
2018-06-02 12:16:14 +0300

    patched veiws together with the urls and apropriate html files, inputed the '/static/' tag for the images wherever needed.

--- auctionsonline/website/urls.py
+++ auctionsonline/website/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name = 'website'
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('', views.register, name='register'),
+	path('',views.login, name='login'),
+    path('',views.profile, name='profile'),
+    path('',views.bid, name='bid'),
+
 ]
--- auctionsonline/website/views.py
+++ auctionsonline/website/views.py
@@ -5,3 +5,15 @@ from django.template import loader
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def bid(request):
+    return render(request, 'bid.html')
